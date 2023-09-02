2c44632d174f095a0c1c741509a9fae00aced790
dylanlyq <dylanlyq@gmail.com>
2023-09-02 17:26:36 +0800

    Updated Bachalier Model with formulas

--- Part 1 - Analytical Option Formulae/option_types/option_models/bachelier_model.py
+++ Part 1 - Analytical Option Formulae/option_types/option_models/bachelier_model.py
@@ -7,28 +7,60 @@ from .abstract_option_model import AbstractOptionModel
 
 
 class AbstractBachelierModel(AbstractOptionModel):
-    pass  # TODO: add __init__
+    def __init__(self, parameters):
+        super().__init__(parameters)
+        self.calculate_d1_d2()
+
+    def calculate_d1_d2(self):
+        S = self.parameters['S']
+        K = self.parameters['K']
+        T = self.parameters['T']
+        r = self.parameters['r']
+        σ = self.parameters['σ']
+        self.d1 = (S - K) / (σ * math.sqrt(T)) + 0.5 * σ * math.sqrt(T)
+        self.d2 = self.d1 - σ * math.sqrt(T)
+        self.N_d1 = norm.cdf(self.d1)
+        self.N_d2 = norm.cdf(self.d2)
+        self.N_minus_d1 = norm.cdf(-self.d1)
+        self.N_minus_d2 = norm.cdf(-self.d2)
+        return S, K, r, T, σ, self.N_d1, self.N_d2, self.N_minus_d1, self.N_minus_d2
 
 
+    
 class VanillaBachelierModel(AbstractBachelierModel):
     def calculate_call_price(self) -> float:
-        pass  # TODO: add formulae
+        S, K, _, _, _, self.N_d1, self.N_d2, _, _, = self.calculate_d1_d2()
+        call_price = math.exp(-r * T) * \
+            (S * self.N_d1 - K * self.N_d2)
+        return call_price
 
     def calculate_put_price(self) -> float:
-        pass  # TODO: add formulae
+        S, K, _, _, _, _, _, self.N_minus_d1, _ = self.calculate_d1_d2()
+        put_price = math.exp(-r * T) * \
+            (K * self.N_minus_d2 - S * self.N_minus_d1)
+        return put_price
 
 
 class DigitalCashOrNothingBachelierModel(AbstractBachelierModel):
     def calculate_call_price(self) -> float:
-        pass  # TODO: add formulae
+        _, _, r, T, _, self.N_d1, _, _, _ = self.calculate_d1_d2()
+        cash_amount = self.parameters['cash_amount']
+        call_price = math.exp(-r * T) * (cash_amount * self.N_d1)
+        return call_price
 
     def calculate_put_price(self) -> float:
-        pass  # TODO: add formulae
-
+        _, _, r, T, _, _, _,self.N_minus_d1, _ = self.calculate_d1_d2()
+        cash_amount = self.parameters['cash_amount']
+        put_price = math.exp(-r * T) * (cash_amount * self.N_minus_d1)
+        return put_price
 
 class DigitalAssetOrNothingBachelierModel(AbstractBachelierModel):
     def calculate_call_price(self) -> float:
-        pass  # TODO: add formulae
+        S, _, r, T, _, self.N_d1, _, _, _ = self.calculate_d1_d2()
+        call_price = math.exp(-r * T) * self.N_d1
+        return call_price
 
     def calculate_put_price(self) -> float:
-        pass  # TODO: add formulae
+        S, _, r, T, _, _, self.N_minus_d1, _, _ = self.calculate_d1_d2()
+        put_price = math.exp(-r * T) * self.N_minus_d1
+        return put_price
