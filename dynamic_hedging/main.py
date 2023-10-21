91eac8f4636084ce782e7b3e8738b4b54f9b05f1
andreignatius <andre.callme@gmail.com>
2023-10-22 01:40:07 +0800

    optimized main file and deleted WIP main_optimized

--- dynamic_hedging/main.py
+++ dynamic_hedging/main.py
@@ -5,25 +5,22 @@ import time
 
 def simulate_paths(S0, sigma, r, T, N, n_paths):
     dt = T/N
+    dW = np.random.randn(N, n_paths) * np.sqrt(dt)  # Array of Wiener increments
     paths = np.zeros((N+1, n_paths))
     paths[0] = S0
     for t in range(1, N+1):
-        Z = np.random.randn(n_paths)
-        paths[t] = paths[t-1] * np.exp((r - 0.5*sigma**2)*dt + sigma*np.sqrt(dt)*Z)
+        paths[t] = paths[t-1] * np.exp((r - 0.5*sigma**2)*dt + sigma*dW[t-1])
     return paths
 
-def compute_phi(St, K, r, sigma, t, T):
-    if t >= T:
-        return 1.0 if St > K else 0.0
-    d1 = (np.log(St/K) + (r + 0.5*sigma**2)*(T-t)) / (sigma*np.sqrt(T-t))
+def compute_phi(paths, K, r, sigma, t, T):
+    dt = T/paths.shape[0]
+    d1 = (np.log(paths[t]/K) + (r + 0.5*sigma**2)*(T-t*dt)) / (sigma*np.sqrt(T-t*dt))
     return norm.cdf(d1)
 
-def compute_psi(St, K, r, sigma, t, T):
-    if t >= T:
-        return 1.0 if St <= K else 0.0
-    
-    d1 = (np.log(St/K) + (r + 0.5*sigma**2)*(T-t)) / (sigma*np.sqrt(T-t))
-    d2 = d1 - sigma*np.sqrt(T-t)
+def compute_psi(paths, K, r, sigma, t, T):
+    dt = T/paths.shape[0]
+    d1 = (np.log(paths[t]/K) + (r + 0.5*sigma**2)*(T-t*dt)) / (sigma*np.sqrt(T-t*dt))
+    d2 = d1 - sigma*np.sqrt(T-t*dt)
     return -K * np.exp(-r*T) * norm.cdf(d2)
 
 # Record the start time
@@ -37,54 +34,34 @@ T = 1/12
 
 # Parameters
 num_paths = 50000
-hedging_intervals = [21, 84]  # N values
-errors = {N: [] for N in hedging_intervals}  # Initialize errors for each N value
+hedging_intervals = [21, 84]  
+errors = {N: [] for N in hedging_intervals}  
 
 for N in hedging_intervals:
-    dt = T/N  # time step
+    dt = T/N  
     
-    for path in range(num_paths):
-        if path % 1000 == 0 and path > 0:
-            print("paths explored: ", path)
-        # Initialization
-        S = S_0
-        B = 1.0  # Starting value for risk-free bond
-        portfolio_value = 0.0  # Reset the portfolio value for each path
-        
-        for t in np.linspace(0, T, N+1)[:-1]:  # Excluding the final time
-            phi = compute_phi(S, K, r, sigma, t, T)
-            psi = compute_psi(S, K, r, sigma, t, T)
-            
-            # Update the portfolio value for this step based on phi and psi
-            portfolio_value = phi * S - psi * B
-            
-            # Simulate the next stock price
-            dS = r * S * dt + sigma * S * np.random.normal(0, np.sqrt(dt))
-            S += dS
-            
-            # Update bond value (assuming continuous compounding)
-            B = B * np.exp(r * dt)
-
-        # At the end of the path (maturity), compute the option payoff
-        option_payoff = max(S - K, 0)
-        
-        # Compute the hedging error for this path
-        hedging_error = portfolio_value - option_payoff
-        
-        # Store the hedging error
-        errors[N].append(hedging_error)
+    paths = simulate_paths(S_0, sigma, r, T, N, num_paths)
+    B = np.exp(r * np.linspace(0, T, N+1))  # Bond value at each time step
     
-    # Now, within the loop, plot the histogram for this N value
-    plt.hist(errors[N], bins=50, alpha=0.5, label=f'N={N}')
+    portfolio_values = np.zeros(num_paths)
+    
+    for t in range(N):  # Loop over time, not paths
+        phi = compute_phi(paths, K, r, sigma, t, T)
+        psi = compute_psi(paths, K, r, sigma, t, T)
+        portfolio_values = phi * paths[t] - psi * B[t]
+    
+    option_payoffs = np.maximum(paths[-1] - K, 0)
+    hedging_errors = portfolio_values - option_payoffs
+    errors[N] = hedging_errors
+    
+    plt.hist(hedging_errors, bins=50, alpha=0.5, label=f'N={N}')
     plt.legend()
 
 print("READY!")
-# Record the end time
 end_time = time.time()
-
-# Calculate and print the elapsed time
 elapsed_time = end_time - start_time
 print(f"Time elapsed: {elapsed_time} seconds")
+plt.xlabel(r'Hedging Error: ( Portfolio Value - Option Payoff )')
+plt.ylabel(r'Frequency Count')
+plt.title(r'Frequency Distribution of Hedging Errors for Different Hedging Intervals')
 plt.show()
-
-
