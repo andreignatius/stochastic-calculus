e5ea4fbd563baae5bf51f6f9cba7d7df35ee485b
Eko Widianto <ekowidianto@outlook.com>
2023-10-22 12:21:20 +0800

    feat: add parameters for various option abstract classes

--- analytical_option_formulae/option_types/vanilla_option.py
+++ analytical_option_formulae/option_types/vanilla_option.py
@@ -9,13 +9,19 @@ class VanillaOption(AbstractOption):
     def black_scholes_model(
         self, S: float, K: float, r: float, sigma: float, T: float
     ) -> AbstractBlackScholesModel:
-        return VanillaBlackScholesModel(S, K, r, sigma, T)  # TODO: Verify params
+        return VanillaBlackScholesModel(S, K, r, sigma, T)
 
-    def bachelier_model(self) -> AbstractBachelierModel:
-        return VanillaBachelierModel()  # TODO: Add params
+    def bachelier_model(
+        self, S: float, K: float, r: float, sigma: float, T: float
+    ) -> AbstractBachelierModel:
+        return VanillaBachelierModel(S, K, r, sigma, T)
 
-    def black_76_model(self) -> AbstractBlack76Model:
-        return VanillaBlack76Model()  # TODO: Add params
+    def black_76_model(
+        self, S: float, K: float, r: float, sigma: float, T: float
+    ) -> AbstractBlack76Model:
+        return VanillaBlack76Model(S, K, r, sigma, T)
 
-    def displaced_diffusion_model(self) -> AbstractDisplacedDiffusionModel:
-        return VanillaDisplacedDiffusionModel()  # TODO: Add params
+    def displaced_diffusion_model(
+        self, S: float, K: float, r: float, sigma: float, T: float, beta: float
+    ) -> AbstractDisplacedDiffusionModel:
+        return VanillaDisplacedDiffusionModel(S, K, r, sigma, T, beta)
