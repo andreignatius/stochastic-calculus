9d62a21655343fa599de4ab4d0ae9cc8fe99fab2
Eko Widianto <ekowidianto@outlook.com>
2023-10-23 22:26:05 +0800

    style: fix formatting

--- dynamic_hedging/main.py
+++ dynamic_hedging/main.py
@@ -3,25 +3,35 @@ import matplotlib.pyplot as plt
 from scipy.stats import norm
 import time
 
+
 def simulate_paths(S0, sigma, r, T, N, n_paths):
-    dt = T/N
+    dt = T / N
     dW = np.random.randn(N, n_paths) * np.sqrt(dt)  # Array of Wiener increments
-    paths = np.zeros((N+1, n_paths))
+    paths = np.zeros((N + 1, n_paths))
     paths[0] = S0
-    for t in range(1, N+1):
-        paths[t] = paths[t-1] * np.exp((r - 0.5*sigma**2)*dt + sigma*dW[t-1])
+    for t in range(1, N + 1):
+        paths[t] = paths[t - 1] * np.exp(
+            (r - 0.5 * sigma**2) * dt + sigma * dW[t - 1]
+        )
     return paths
 
+
 def compute_phi(paths, K, r, sigma, t, T):
-    dt = T/paths.shape[0]
-    d1 = (np.log(paths[t]/K) + (r + 0.5*sigma**2)*(T-t*dt)) / (sigma*np.sqrt(T-t*dt))
+    dt = T / paths.shape[0]
+    d1 = (np.log(paths[t] / K) + (r + 0.5 * sigma**2) * (T - t * dt)) / (
+        sigma * np.sqrt(T - t * dt)
+    )
     return norm.cdf(d1)
 
+
 def compute_psi(paths, K, r, sigma, t, T):
-    dt = T/paths.shape[0]
-    d1 = (np.log(paths[t]/K) + (r + 0.5*sigma**2)*(T-t*dt)) / (sigma*np.sqrt(T-t*dt))
-    d2 = d1 - sigma*np.sqrt(T-t*dt)
-    return -K * np.exp(-r*(T-t*dt)) * norm.cdf(d2)
+    dt = T / paths.shape[0]
+    d1 = (np.log(paths[t] / K) + (r + 0.5 * sigma**2) * (T - t * dt)) / (
+        sigma * np.sqrt(T - t * dt)
+    )
+    d2 = d1 - sigma * np.sqrt(T - t * dt)
+    return -K * np.exp(-r * T) * norm.cdf(d2)
+
 
 # Record the start time
 start_time = time.time()
@@ -30,58 +40,60 @@ S_0 = 100
 K = 100
 sigma = 0.2
 r = 0.05
-T = 1/12
+T = 1 / 12
 
 # Parameters
 num_paths = 50000
-hedging_intervals = [21, 84]  
-errors = {N: [] for N in hedging_intervals}  
+hedging_intervals = [21, 84]
+errors = {N: [] for N in hedging_intervals}
+
 
 def bs_call_price(S, K, r, sigma, T):
-    d1 = (np.log(S/K) + (r + 0.5*sigma**2)*T) / (sigma*np.sqrt(T))
-    d2 = d1 - sigma*np.sqrt(T)
-    return S * norm.cdf(d1) - K * np.exp(-r*T) * norm.cdf(d2)
+    d1 = (np.log(S / K) + (r + 0.5 * sigma**2) * T) / (sigma * np.sqrt(T))
+    d2 = d1 - sigma * np.sqrt(T)
+    return S * norm.cdf(d1) - K * np.exp(-r * T) * norm.cdf(d2)
+
 
 for N in hedging_intervals:
-    dt = T/N  
-    
+    dt = T / N
+
     paths = simulate_paths(S_0, sigma, r, T, N, num_paths)
-    
-    portfolio_values = bs_call_price(S_0, K, r, sigma, T)  # Initial portfolio value when selling the call
-    delta_initial = compute_phi(paths, K, r, sigma, 0, T)
-    
-    # Initial positions in stock and bond
-    stock_position = delta_initial * paths[0]
-    bond_position = portfolio_values - stock_position  # Everything not invested in stock is invested in bond
-    
-    for t in range(1, N):  
+    B = np.exp(r * np.linspace(0, T, N + 1))  # Bond value at each time step
+
+    portfolio_values = bs_call_price(
+        S_0, K, r, sigma, T
+    )  # Initial portfolio value when selling the call
+
+    cash = (
+        portfolio_values
+        - compute_phi(paths, K, r, sigma, 0, T) * paths[0]
+        + compute_psi(paths, K, r, sigma, 0, T)
+    )  # Adjusted cash position
+
+    for t in range(1, N):  # Start from 1 as we've already initialized at t=0
+        delta_prev = compute_phi(paths, K, r, sigma, t - 1, T)
         delta_now = compute_phi(paths, K, r, sigma, t, T)
-        
-        # Calculate change in stock position based on new delta
-        change_in_stock = (delta_now - delta_initial) * paths[t]
-        
-        # Update bond position: account for interest and change in stock position
-        bond_position = bond_position * np.exp(r*dt) - change_in_stock
-        
-        # Update stock position
-        stock_position = delta_now * paths[t]
-        
-        delta_initial = delta_now
-    
+
+        # Adjust portfolio for change in stock and bond positions
+        cash -= (delta_now - delta_prev) * paths[t]
+        cash *= np.exp(r * dt)  # Account for risk-free interest on cash
+
+        portfolio_values = cash + delta_now * paths[t]  # Update portfolio value
+
     option_payoffs = np.maximum(paths[-1] - K, 0)
-    
+
     # Total portfolio value = stock position + bond position
     portfolio_values = stock_position + bond_position
     hedging_errors = portfolio_values - option_payoffs
     errors[N] = hedging_errors
-    
-    plt.hist(hedging_errors, bins=50, alpha=0.5, label=f'N={N}')
+
+    plt.hist(hedging_errors, bins=50, alpha=0.5, label=f"N={N}")
     plt.legend()
 
 end_time = time.time()
 elapsed_time = end_time - start_time
 print(f"Time elapsed: {elapsed_time} seconds")
-plt.xlabel(r'Hedging Error: ( Portfolio Value - Option Payoff ) (\$)')
-plt.ylabel(r'Frequency Count')
-plt.title(r'Frequency Distribution of Hedging Errors for Different Hedging Intervals')
+plt.xlabel(r"Hedging Error: ( Portfolio Value - Option Payoff ) (\$)")
+plt.ylabel(r"Frequency Count")
+plt.title(r"Frequency Distribution of Hedging Errors for Different Hedging Intervals")
 plt.show()
