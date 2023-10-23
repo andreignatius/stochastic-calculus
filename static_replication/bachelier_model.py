61c71293d3e2dd1da7376a921870e58bba5c1aa2
andreignatius <andre.callme@gmail.com>
2023-10-23 19:45:00 +0800

    fix a few careless bug in bachelier_model and also completed for payoff function

--- static_replication/bachelier_model.py
+++ static_replication/bachelier_model.py
@@ -11,10 +11,17 @@ import math
 
 from analytical_option_formulae.option_types.option_models.bachelier_model import VanillaBachelierModel
 
+# Start Date: 1-Dec-2020
+# End Date: 15-Jan-2021
+# The time difference is 45 days (considering 31 days in December and 14 days in January).
+
+# In a year with 365 days, the time to maturity T is: 45 / 365
+
 # Example:
-S = 100  # Stock price today
-K = 110  # Strike price
-T = 1  # Time until expiry (in years)
+S = 100.0  # Stock price today
+K = 110.0  # Strike price
+T = 45/365  # Time until expiry (in years)
+# T = 1
 r = 0.05  # Risk-free rate
 sigma = 0.4  # Volatility
 
@@ -23,30 +30,41 @@ vanillaBachelier = VanillaBachelierModel(S, K, T, r, sigma*S)
 print("Bachelier call price: ", vanillaBachelier.calculate_call_price())
 print("Bachelier put price: ", vanillaBachelier.calculate_put_price())
 
+
+F = S * np.exp(r*T)
+
 def callintegrand_Bachelier(K):
-    # vanillaBachelier.K = K
     vanillaBachelier = VanillaBachelierModel(S, K, T, r, sigma*S)
     price = vanillaBachelier.calculate_call_price()
     return price / K**2
 
 def putintegrand_Bachelier(K):
-    # vanillaBachelier.K = K
     vanillaBachelier = VanillaBachelierModel(S, K, T, r, sigma*S)
     price = vanillaBachelier.calculate_put_price()
     return price / K**2
 
-F = S + np.exp(r*T)
+I_put = quad(putintegrand_Bachelier, 0, F)
+I_call = quad(callintegrand_Bachelier, F, 5000)
 
-I_put = quad(lambda x: putintegrand_Bachelier(x), 0.0, F)
-I_call = quad(lambda x: callintegrand_Bachelier(x), F, 5000)
-E_var = 2 * (I_put[0] + I_call[0])
+E_var = 2*np.exp(r*T)*(I_put[0] + I_call[0])
 
+print('The expected integrated variance is: %.9f' % E_var)
 
-print('The expected integrated variance is 000: %.9f' % E_var)
+sigma = math.sqrt( E_var / T)
+print('What sigma should we use: ', sigma)
 
-sigma = math.sqrt(E_var)
-print('What sigma should we use: %.9f' % sigma)
+def exotic_payoff_replication(S_T):
+    return S_T**(1/3) + 1.5 * np.log(S_T) + 10.0
 
-vanillaBachelier = VanillaBachelierModel(S, K, T, r, sigma*S)
-print("Bachelier call price: ", vanillaBachelier.calculate_call_price())
-print("Bachelier put price: ", vanillaBachelier.calculate_put_price())
+def exotic_option_price():
+    # Let's consider a dynamic range for integration.
+    # Setting the range based on multiple standard deviations around the forward price.
+    S_min = F - 10 * sigma * np.sqrt(T)
+    S_max = F + 10 * sigma * np.sqrt(T)
+    
+    integrand = lambda S_T: exotic_payoff_replication(S_T) * norm.pdf((S_T - F) / (sigma * np.sqrt(T)))
+    price, _ = quad(integrand, S_min, S_max, epsabs=1e-8, epsrel=1e-8)
+    
+    return price * np.exp(-r * T)
+
+print('The exotic option price using static replication is: %.2f' % exotic_option_price())
--- static_replication/black_scholes_model.py
+++ static_replication/black_scholes_model.py
@@ -11,18 +11,24 @@ import math
 
 from analytical_option_formulae.option_types.option_models.black_scholes_model import VanillaBlackScholesModel
 
+# Start Date: 1-Dec-2020
+# End Date: 15-Jan-2021
+# The time difference is 45 days (considering 31 days in December and 14 days in January).
+
+# In a year with 365 days, the time to maturity T is: 45 / 365
+
 # Example:
 S = 100  # Stock price today
 K = 110  # Strike price
-T = 1  # Time until expiry (in years)
+# T = 1  # Time until expiry (in years)
+T = 45/365  # Time until expiry (in years)
 r = 0.05  # Risk-free rate
 sigma = 0.4  # Volatility
+F = S * np.exp(r*T)
 
 vanillaBSM = VanillaBlackScholesModel(S, K, r, sigma, T)
 print("BSM call price: ", vanillaBSM.calculate_call_price())
 print("BSM put price: ", vanillaBSM.calculate_put_price())
-
-F = S + np.exp(r*T)
 
 def callintegrand_BSM(K):
     vanillaBSM = VanillaBlackScholesModel(S, K, r, sigma, T)
@@ -51,22 +57,45 @@ def putintegrand_BSM(K):
     price = vanillaBSM.calculate_put_price()
     return price / K**2
 
-# S = 100.0
-# r = 0.05
-# T = 1.0
-# sigma = 0.4
-F = S * np.exp(r*T)
+
+
 I_put = quad(lambda x: putintegrand_BSM(x), 0.0, F)
 I_call = quad(lambda x: callintegrand_BSM(x), F, 5000)
 E_var = 2*np.exp(r*T)*(I_put[0] + I_call[0])
 print('The expected integrated variance is 111: %.9f' % E_var)
 
-sigma = math.sqrt(E_var)
-print('What sigma should we use: %.9f' % sigma)
-
 vanillaBSM = VanillaBlackScholesModel(S, K, r, sigma, T)
 print("BSM call price: ", vanillaBSM.calculate_call_price())
 print("BSM put price: ", vanillaBSM.calculate_put_price())
+
+sigma = math.sqrt( E_var / T)
+print('What sigma should we use: ', sigma)
+
+def exotic_payoff_replication(S_T):
+    return S_T**(1/3) + 1.5 * np.log(S_T) + 10.0
+
+def exotic_option_price():
+    # Define the Black-Scholes density function
+    def BS_density(S_T):
+        term1 = np.log(S_T/S)
+        term2 = (r - 0.5 * sigma**2) * T
+        numerator = np.exp(- (term1 - term2)**2 / (2 * sigma**2 * T))
+        denominator = S_T * sigma * np.sqrt(2 * np.pi * T)
+        return numerator / denominator
+
+    # We consider a dynamic range for integration.
+    # Setting the range based on multiple standard deviations around the forward price.
+    F = S * np.exp(r * T)
+    S_min = S * np.exp((r - sigma**2 / 2) * T - 6 * sigma * np.sqrt(T))
+    S_max = S * np.exp((r - sigma**2 / 2) * T + 6 * sigma * np.sqrt(T))
+    
+    integrand = lambda S_T: exotic_payoff_replication(S_T) * BS_density(S_T)
+    price, _ = quad(integrand, S_min, S_max, epsabs=1e-8, epsrel=1e-8)
+    
+    return price * np.exp(-r * T)
+
+print('The exotic option price using static replication is: %.2f' % exotic_option_price())
+
 
 # Prof Tee method for comparison
 
@@ -95,7 +124,8 @@ def putintegrand(K, S, r, T, sigma):
 S = 100.0
 K = 110.0
 r = 0.05
-T = 1.0
+# T = 1.0
+T = 45/365  # Time until expiry (in years)
 sigma = 0.4
 F = S * np.exp(r*T)
 I_put = quad(lambda x: putintegrand(x, S, r, T, sigma), 0.0, F)
